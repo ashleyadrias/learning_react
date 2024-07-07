349a728d4778be880dce225cae69ec679a356fde
ashleyadrias <agadrias@gmail.com>
2024-07-07 11:44:49 -0700

    learned about .map in react

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -20,35 +20,52 @@ const Part = (props) => {
   )
 }
 
+
+
 const Content = (props) => {
   console.log(props)
   return (
     <div>
-        <Part part = {props.part} exercise = {props.exercise} />
+      {Array.isArray(props.parts) && props.parts.map((value, index) => (
+        <Part
+          key={index}
+          part={value.name}
+          exercise={value.exercises}
+        />
+      ))}
     </div>
   )
 }
 
 
 const App = () => {
-  // const-definitions
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header  course  = {course} />
-      <Content part = {part1} exercise = {exercises1} />
-      <Content part = {part2} exercise = {exercises2} />
-      <Content part = {part3} exercise = {exercises3} />
+      <Header course={course} />
+      <Content parts={parts} /> 
+      {/* <Total parts={parts} /> */}
     </div>
   )
 }
 
 export default App
+
+
+
 
