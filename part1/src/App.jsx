b15a26c393667e727ebf2ff437579a41300d6071
agadrias@gmail.com <agadrias@gmail.com>
2024-07-14 10:22:35 -0700

    react axios, promisses, effect-hooks

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,26 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-import {useState} from 'react'
 
 const App = (props) => {
 
     // STATES
-    const [notes, setNotes] = useState(props.notes)
-
+    const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
+    const [showAll, setShowAll] = useState(true)
+
+    // EFFECT-HOOKS
+    useEffect(() => {
+        console.log('effect')
+      
+        const eventHandler = response => {
+          console.log('promise fulfilled')
+          setNotes(response.data)
+        }
+      
+        const promise = axios.get('http://localhost:3001/notes')
+        promise.then(eventHandler)
+      }, [])
 
     // EVENT HANDLERS
     const addNote = (event) => {
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -3,46 +3,55 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+// import axios from 'axios'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} course={course}/>
-)
+// axios.get('http://localhost:3001/notes').then(response => {
+//   const notes = response.data
+//   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+// })
+
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
+
+// const course = {
+//     id: 1,
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10,
+//         id: 1
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7,
+//         id: 2
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14,
+//         id: 3
+//       }
+//     ]
+//   }
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <App notes={notes} course={course}/>
+// )
