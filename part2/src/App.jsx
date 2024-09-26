938f356a41fd54978cd4808b4fdf91e5f604e469
Ashley Adrias <ashleyadrias@Ashleys-MBP.lan>
2024-09-26 11:17:55 -0700

    move all functions out of return

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Note from './components/Note'
 
-//states
+
 const App = (props) => {
+  //states
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
@@ -11,36 +12,49 @@ const App = (props) => {
     ? notes
     : notes.filter(note => note.important === true)
 
-//event handlers
-const addNote = (event) => {
-  event.preventDefault()
-  const noteObject = {
-    content: newNote,
-    important: Math.random() < 0.5,
-    id: String(notes.length + 1),
+    const buttonLabel = showAll 
+    ? 'important' 
+    : 'all';
+
+  //event handlers
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  setNotes(notes.concat(noteObject))
-  setNewNote('')
-}
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
-const handleNoteChange = (event) => {
-  console.log(event.target.value)
-  setNewNote(event.target.value)
-}
+  const handleButtonClick = () => {
+    setShowAll(!showAll);
+  }
+
+  // functions
+  const renderNotes = () => {
+    return notesToShow.map(note => (
+      <Note key={note.id} note={note} />
+    ))
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
-        <button onClick = {() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
+        <button onClick={handleButtonClick}>
+          show {buttonLabel}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {renderNotes()}
       </ul>
       <form onSubmit={addNote}>
         <input 
