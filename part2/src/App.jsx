92df0b98021cf84f44f210c102ecf89e7011a622
Ashley Adrias <ashleyadrias@Ashleys-MacBook-Pro.local>
2024-10-02 16:19:29 -0700

    adding part2 files " ""

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 
 const App = (props) => {
   //states
-  const [notes, setNotes] = useState(props.notes)
+  //const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
 
@@ -15,6 +17,19 @@ const App = (props) => {
     const buttonLabel = showAll 
     ? 'important' 
     : 'all';
+
+  // axios, api call, hooks
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
 
   //event handlers
   const addNote = (event) => {
