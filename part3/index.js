bc786f4930342057a334ce25e2249257d7a37ee2
Ashley Adrias <ashleyadrias@Ashleys-MacBook-Pro.local>
2024-10-08 23:09:42 -0700

    last of part3

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,7 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -17,16 +20,40 @@ let notes = [
       important: true
     }
   ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/notes', (request, response) => {
+    response.json(notes)
   })
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
-})
+  app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+  })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
+
+  app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
